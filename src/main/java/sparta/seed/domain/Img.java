cee4977b7a43417e68601a7a9a264b5007b09d21
2m <philomonx1@gmail.com>
2022-09-02 15:30:22 +0900

    [feat] 인증글(replay) CRUD 완료, 좋아요 완료

--- src/main/java/sparta/seed/domain/Img.java
+++ src/main/java/sparta/seed/domain/Img.java
@@ -25,7 +25,7 @@ public class Img extends Timestamped {
   @JoinColumn(name = "articleId")
   private Article article;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
   @JsonBackReference
   @JoinColumn(name = "replayId")
   private Replay replay;
