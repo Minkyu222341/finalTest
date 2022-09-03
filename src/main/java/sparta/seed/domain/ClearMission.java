cb62f15c9357e9bd26d02111d92477696f32d778
2m <philomonx1@gmail.com>
2022-09-03 22:14:58 +0900

    [feat] 개인미션 구현 완료

--- src/main/java/sparta/seed/domain/ClearMission.java
+++ src/main/java/sparta/seed/domain/ClearMission.java
@@ -1,9 +1,15 @@
 package sparta.seed.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sparta.seed.util.Timestamped;
 
 import javax.persistence.*;
 
+
+@Getter
+@NoArgsConstructor
 @Entity
 public class ClearMission extends Timestamped {
   //PK
@@ -15,4 +21,9 @@ public class ClearMission extends Timestamped {
   //완료한 미션
   private String content;
 
+  @Builder
+  public ClearMission(Long memberId, String content) {
+    this.memberId = memberId;
+    this.content = content;
+  }
 }
