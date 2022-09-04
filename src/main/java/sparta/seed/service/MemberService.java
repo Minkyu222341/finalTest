c9c33015fb1a111d59867eae7bca97a67d457bd9
2m <philomonx1@gmail.com>
2022-09-04 18:20:41 +0900

    [feat] 미션 클리어 갯수에 따라 레벨 변화 완성

--- src/main/java/sparta/seed/service/MemberService.java
+++ src/main/java/sparta/seed/service/MemberService.java
@@ -9,6 +9,7 @@ import sparta.seed.domain.Member;
 import sparta.seed.domain.dto.requestDto.SocialMemberRequestDto;
 import sparta.seed.domain.dto.responseDto.CommunityResponseDto;
 import sparta.seed.domain.dto.responseDto.MemberResponseDto;
+import sparta.seed.repository.ClearMissionRepository;
 import sparta.seed.repository.CommunityRepository;
 import sparta.seed.repository.MemberRepository;
 import sparta.seed.sercurity.UserDetailsImpl;
@@ -24,6 +25,7 @@ import java.util.Optional;
 public class MemberService {
   private final MemberRepository memberRepository;
   private final CommunityRepository communityRepository;
+  private final ClearMissionRepository clearMissionRepository;
   private final DateUtil dateUtil;
 
   /**
@@ -31,14 +33,16 @@ public class MemberService {
    */
   public ResponseEntity<MemberResponseDto> getMyPage(UserDetailsImpl userDetails) {
     Optional<Member> member = memberRepository.findById(userDetails.getId());
+
+    int clearMission = clearMissionRepository.countAllByMemberId(member.get().getId());
+    int missionDiv = clearMission / 5;
+
     MemberResponseDto memberResponseDto = MemberResponseDto.builder()
             .id(member.get().getId())
             .nickname(member.get().getNickname())
             .profileImage(member.get().getProfileImage())
-            .totalClear(52) // 미션 DB에서 멤버의 PK를 전부 카운팅해서 갯수를 리턴
-            .level(member.get().getLevel())
-            .exp(member.get().getExp())
-            .nextLevelExp(20) //로직 아직 고민중
+            .totalClear(clearMission) // 미션 DB에서 멤버의 PK를 전부 카운팅해서 갯수를 리턴
+            .level((missionDiv + 1)) // 5개를 완료하면 레벨이 1오름 ... 1부터 시작한다고 할 때 만렙은 11이 될것
             .build();
     return ResponseEntity.ok().body(memberResponseDto);
   }
